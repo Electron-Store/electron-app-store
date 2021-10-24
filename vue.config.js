2edc3d9b7ed21a69651c06f851372ad161bf9bf1
Patrick-web <pntxall100@gmail.com>
2021-10-24 11:19:42 +0300

    v0.0.4

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,25 @@
 module.exports = {
-    pluginOptions: {
-        electronBuilder: {
-            nodeIntegration: true,
-            builderOptioons: {
-                publish: [{
-                    provider: "github",
-                    owner: "Electron-Store",
-                    repo: "electron-app-store"
-                }]
-            },
-        },
-    },
+	pluginOptions: {
+		electronBuilder: {
+			nodeIntegration: true,
+			mainProcessFile: "main/background.js",
+			builderOptions: {
+				appId: "electron-store",
+				productName: "EA Store",
+				publish: [
+					{
+						provider: "github",
+						owner: "Electron-Store",
+						repo: "electron-app-store",
+					},
+				],
+				snap: {
+					title: "Electron Store",
+					confinement: "classic",
+					summary: "The Internet's App Store",
+					grade: "devel",
+				},
+			},
+		},
+	},
 };
