1906b45385fe32fcf21407c2e4ac2be55b420845
Rajdeeep Malakar <Rajdeep@tgeeks.cf>
2021-12-03 15:17:36 +0530

    Moved API from Heroku to Custom Server
    Signed-off-by: Rajdeeep Malakar <Rajdeep@tgeeks.cf>

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,22 +1,22 @@
 export async function createRequest(route = "", data = "") {
-	const requestOptions = {
-		method: "GET",
-		redirect: "follow",
-	};
+    const requestOptions = {
+        method: "GET",
+        redirect: "follow",
+    };
 
-	const response = await fetch(
-		`https://electron-app-store.herokuapp.com/${route}?${data}`,
-		requestOptions
-	);
-	const responseData = await response.json();
-	return responseData;
+    const response = await fetch(
+        `https://api.electron-store.org/${route}?${data}`,
+        requestOptions
+    );
+    const responseData = await response.json();
+    return responseData;
 }
 
 export function toQueryString(obj) {
-	let str = [];
-	for (var p in obj)
-		if (obj.hasOwnProperty(p)) {
-			str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
-		}
-	return str.join("&");
+    let str = [];
+    for (var p in obj)
+        if (obj.hasOwnProperty(p)) {
+            str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+        }
+    return str.join("&");
 }
